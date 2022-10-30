c83a1e95868903e4b92f12be4e979e691af8e9be
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-10-30 12:25:07 -0400

    Initial Commit

---
+++ LearningApp-CWC-5/LearningApp_CWC_5App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp_CWC_5App.swift
+//  LearningApp-CWC-5
+//
+//  Created by Kevin Mattocks on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp_CWC_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
