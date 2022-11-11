2e7333217daa629855243e03b38edab9af20b581
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-11-10 20:02:15 -0500

    Parse the JSON data in the ContentViewModel Parse the style data in the ContentViewModel

--- LearningApp-CWC-5/ViewModel/ContentViewModel.swift
+++ LearningApp-CWC-5/ViewModel/ContentViewModel.swift
@@ -11,4 +11,57 @@ import Foundation
 
 class ContentViewModel: ObservableObject {
     
+    //Storing our modules in. Module is an array since the JSON data is surrounded by square brackets
+    @Published var modules = [Module]()
+    
+    
+    //Style property. Setting it to an optional Data object
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    
+    func getLocalData() {
+        //Get URL to JSON file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        //Read the file into a data object
+        do {
+            
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            
+            //Try to decode JSON into an array of modules
+            let jsonDecoder = JSONDecoder()
+            
+            
+           var modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            //Assign parsed modules to modules property
+            self.modules = modules
+            
+        } catch {
+            print("Couldn't parse local data")
+        }
+        
+        //MARK: - Parse the style data
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+            
+            
+        } catch {
+            print("Could not parse the style dta")
+        }
+
+    }
+    
+    
+    
+    
 }
