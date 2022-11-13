93806d1ddf3e6a2a69b98517776c50de269f52ab
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-11-13 18:54:14 -0500

    Create UI for the HomeView displaying the Lesson card and the Testing card Extract code to reuse for the cards and name it HomeViewRow. Pass in variables to make it dynamic

--- LearningApp-CWC-5/Views/HomeView.swift
+++ LearningApp-CWC-5/Views/HomeView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentViewModel
+    
     var body: some View {
-       Text("CodeWithChris Learning App Module!")
+        NavigationView {
+            
+            VStack(alignment: .leading) {
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                
+                ScrollView {
+                    LazyVStack {
+                        ForEach(model.modules) {
+                            module in
+                            
+                            //MARK: - Learning Card
+                            VStack(spacing: 20) {
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                
+                                //MARK: - Testing Card
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Lessons", time: module.test.time)
+                            }
+                            
+                            
+                            
+                          
+                        }
+                        
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentViewModel())
     }
 }
